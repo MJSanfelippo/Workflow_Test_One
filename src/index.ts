6235e067ae6018c280eb70cb8d60db4b00f6a07c
MSanfelippo <michael.sanfelippo@baytechconsulting.com>
2019-11-12 19:15:50 -0800

    maybe last update?

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 import {PLATFORM} from 'aurelia-pal';
-import { WorkflowManager } from 'WorkflowManager';
+import { WorkflowManager } from './WorkflowManager';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
