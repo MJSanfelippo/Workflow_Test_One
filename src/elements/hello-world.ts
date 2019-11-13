c90120fdaeb6d75ce8bb54fe5f7fbbd1a673e7d2
MSanfelippo <michael.sanfelippo@baytechconsulting.com>
2019-11-12 19:22:03 -0800

    added workflow id to test

--- src/elements/hello-world.ts
+++ src/elements/hello-world.ts
@@ -2,4 +2,9 @@ import {bindable} from 'aurelia-framework';
 
 export class HelloWorld {
   @bindable public message: string = '';
+  @bindable workflowId: string;
+
+  attached(){
+    console.log(this.workflowId);
+  }
 }
