881d2ddf5e249a4d689dcf9db81417a3663ff35b
MSanfelippo <michael.sanfelippo@baytechconsulting.com>
2019-11-13 11:04:31 -0800

    change

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
-import {PLATFORM} from 'aurelia-pal';
+import { FrameworkConfiguration } from 'aurelia-framework';
+import { PLATFORM } from 'aurelia-pal';
 import { WorkflowManager } from './WorkflowManager';
+
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('./elements/workflow-container')
+    PLATFORM.moduleName('./elements/workflow-container'),
+    './WorkflowManager'
   ]);
 
   config.singleton(WorkflowManager, WorkflowManager);
+  config.container.get(WorkflowManager);
 }
 
-export let workflowManager = new WorkflowManager();
+export { WorkflowManager };
