e75da86de52d765389cb35a9c4745172c5ca7182
MSanfelippo <michael.sanfelippo@baytechconsulting.com>
2019-11-13 08:36:50 -0800

    added bindables

--- src/elements/workflow-container.ts
+++ src/elements/workflow-container.ts
@@ -3,6 +3,9 @@ import {bindable} from 'aurelia-framework';
 export class WorkflowContainer {
   @bindable public message: string = '';
   @bindable workflowId: string;
+  @bindable allSchedules: any[];
+  @bindable allMessages: any[];
+  @bindable allRoutingPolicies: any[];
 
   attached(){
     console.log(this.workflowId);
