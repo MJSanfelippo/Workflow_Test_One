22cfc4a0a1f3e8adef4ed8cd4bc05d843f288601
Michael Sanfelippo <michael.sanfelippo@baytechconsulting.com>
2019-11-14 07:24:40 -0800

    remove my obj

--- src/workflowAddContainer.ts
+++ src/workflowAddContainer.ts
@@ -34,9 +34,6 @@ export class WorkflowAddContainer {
   private selectedNode: WorkflowNode;
 
 
-  @bindable myObj: any;
-
-
   constructor(
     private router: Router,
     private utility: Utility,
